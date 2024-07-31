c4587f1cafbf9936d2fd3ec2bf36ec4ec3b067e0
Lê Văn Tú <107534756+levantu2003@users.noreply.github.com>
2024-07-31 07:44:50 +0700

    Home Screen UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import Colors from "@/constants/Colors";
 import { Stack } from "expo-router";
@@ -12,8 +12,19 @@ const Page = () => {
           header: () => <Header />,
         }}
       />
-      <View style={styles.container}>
-        <Text>Page</Text>
+      <View style={[styles.container, { paddingTop: 70 }]}>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={{ gap: 10 }}>
+            <Text style={{ color: Colors.white, fontSize: 16 }}>
+              <Text style={{ fontWeight: 700 }}>Chi tiêu</Text> của tôi
+            </Text>
+            <Text
+              style={{ color: Colors.white, fontSize: 22, fontWeight: 700 }}
+            >
+              24.758.000 VND
+            </Text>
+          </View>
+        </ScrollView>
       </View>
     </>
   );
